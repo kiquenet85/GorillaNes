acffa80d1e74fa02ca3bbcd48c7630b4d0ec07f9
nesdupier <kiquenet85@gmail.com>
2021-03-22 23:08:48 -0500

    ADd photo to post with correct permissions.

--- app/src/main/java/com/nesgorilla/base/BaseFragment.kt
+++ app/src/main/java/com/nesgorilla/base/BaseFragment.kt
@@ -1,17 +1,32 @@
 package com.nesgorilla.base
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.MediaStore
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.nesgorilla.R
+import com.nesgorilla.base.error.ErrorHandler
 import com.nesgorilla.di.AppComponent
 import com.nesgorilla.manager.ResourceManager
+import java.io.File
 
-open class BaseFragment : Fragment(){
-    protected val resourceManager: ResourceManager = AppComponent.getInstance().provideResourceManager()
 
-    open fun clearKeyboardFromScreen(activity: Activity) {
+open class BaseFragment : Fragment() {
+    protected val resourceManager: ResourceManager =
+        AppComponent.getInstance().provideResourceManager()
+
+    protected val errorHandler: ErrorHandler =
+        AppComponent.getInstance().provideErrorHandler()
+
+    protected open fun clearKeyboardFromScreen(activity: Activity) {
         if (activity.window.currentFocus != null) {
             val imm = activity.getSystemService(
                 Context.INPUT_METHOD_SERVICE
@@ -21,7 +36,105 @@ open class BaseFragment : Fragment(){
         activity.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
     }
 
-    open fun keyboardResizeMode(activity: Activity) {
+    protected open fun keyboardResizeMode(activity: Activity) {
         activity.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+    }
+
+    protected open fun requestDevicePhoto() {
+        if (verifyStoragePermissions()) {
+            val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+            getIntent.type = "image/*"
+
+            val pickIntent =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            pickIntent.type = "image/*"
+
+            val chooserIntent =
+                Intent.createChooser(getIntent, resourceManager.getString(R.string.select_image))
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(pickIntent))
+
+            startActivityForResult(chooserIntent, IMAGE_PICKER_SELECT)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_PICKER_SELECT) {
+            if (resultCode != Activity.RESULT_OK) {
+                return
+            }
+            val uri = data?.data
+            if (uri != null) {
+                uriToImageFile(uri)?.let {
+                    setPickedImageToImageView(it)
+                }
+            }
+        }
+    }
+
+    private fun uriToImageFile(uri: Uri): File? {
+        val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = requireActivity().contentResolver.query(uri, filePathColumn, null, null, null)
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                val columnIndex = cursor.getColumnIndex(filePathColumn[0])
+                val filePath = cursor.getString(columnIndex)
+                cursor.close()
+                return File(filePath)
+            }
+            cursor.close()
+        }
+        return null
+    }
+
+    protected open fun setPickedImageToImageView(filePath: File) {
+    }
+
+    open fun verifyStoragePermissions() : Boolean {
+        // Check if we have write permission
+        val permission = ActivityCompat.checkSelfPermission(
+            requireActivity(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+       return if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            )
+            false
+        } else true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_EXTERNAL_STORAGE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    requestDevicePhoto()
+                } else {
+                    Toast.makeText(requireContext(), "Not available to attach system photos.", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+    }
+}
+
+    companion object {
+        private val PERMISSIONS_STORAGE = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        const val IMAGE_PICKER_SELECT = 99
+        const val REQUEST_EXTERNAL_STORAGE = 98
     }
 }
--- app/src/main/java/com/nesgorilla/feature/post/ui/CreatePostFragment.kt
+++ app/src/main/java/com/nesgorilla/feature/post/ui/CreatePostFragment.kt
@@ -2,16 +2,19 @@ package com.nesgorilla.feature.post.ui
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import com.nesgorilla.R
 import com.nesgorilla.base.BaseFragment
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import java.io.File
+import java.lang.Exception
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -21,6 +24,7 @@ class CreatePostFragment : BaseFragment() {
     private lateinit var post: EditText
     private lateinit var characters: TextView
     private lateinit var addPhoto: Button
+    private lateinit var image: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,10 +39,11 @@ class CreatePostFragment : BaseFragment() {
         post = view.findViewById(R.id.post)
         characters = view.findViewById(R.id.characters)
         addPhoto = view.findViewById(R.id.button)
+        image = view.findViewById(R.id.image)
 
         addPhoto.apply {
             setOnClickListener {
-
+                requestDevicePhoto()
             }
         }
 
@@ -47,4 +52,33 @@ class CreatePostFragment : BaseFragment() {
             characters.setText("$size/${105 - size}")
         }
     }
+
+    override fun setPickedImageToImageView(filePath: File) {
+        super.setPickedImageToImageView(filePath)
+        Picasso.get()
+            .load(filePath)
+            .into(image,object: Callback {
+                override fun onSuccess() {
+
+                }
+
+                override fun onError(e: Exception) {
+                   errorHandler.report(e)
+                }
+            })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.share, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            R.id.action_share -> {
+                //viewModel.selectMultipleItems()
+                true
+            }
+            else -> false
+        }
 }
